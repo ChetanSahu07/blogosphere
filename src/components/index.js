c411fec3da23db39dd01d963268cb76ae7e7935a
ChetanSahu07 <133204061+ChetanSahu07@users.noreply.github.com>
2025-02-10 15:32:20 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
-import Logo from "./logo/logo";
+import Logo from "./logo/Logo";
 import Input from './Input'
 import Login from "./Login";
 import RTE from "./RTE";
